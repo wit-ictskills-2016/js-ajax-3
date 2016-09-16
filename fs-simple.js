c1dfea7ad5d7c53b7c3acbcf8449773f71558713
Eamonn de Leastar <edeleastar@wit.ie>
2016-09-16 09:18:49 +0100

    display checking venues images in card layout

--- fs-simple.js
+++ fs-simple.js
@@ -1,20 +1,36 @@
 var fsCredentials = '&client_id=' + fsConfig.client_id + '&client_secret=' + fsConfig.client_secret + '&v=20140601';
 
-function displayCheckins(checkins) {
+function displayCheckins(checkins, cardSection) {
   for (let checkin of checkins) {
-    $('#venue_table').append(`
-      <tr>
-        <td>  ${checkin.name}  </td>
-        <td>  ${checkin.checkins}  </td>
-        <td>  ${checkin.users} </td>
-        <td>  ${checkin.link}  </td>
-      </tr>`);
+    cardSection.append(`
+        <a class="ui link raised card" href="${this.url}">
+          <div class="ui content">
+            <div class="right floated meta">
+              <div class="ui teal circular label"> ${checkin.rating}</div>
+            </div>
+            <div class="header">${checkin.name}</div>
+          </div>
+          <div class="ui image">
+            <img src="${checkin.imageUrl}">
+          </div>
+          <div class="content">
+          </div>
+          <div class="extra content">
+            <span class="right floated">
+              ${checkin.checkins} Checkins
+            </span>
+            <span>
+              <i class="user icon"></i>
+              ${checkin.users} Users
+            </span>
+          </div>
+        </a>`);
   }
 }
 
-function loadVenues(locationName, venueKeyword) {
+function loadVenues(locationName, venueKeyword, cardSection) {
   var requestOptions = {
-    url: fsConfig.base_url + 'near=' + locationName + '&query=' + venueKeyword + fsCredentials,
+    url: fsConfig.base_url + 'near=' + locationName + '&query=' + venueKeyword + '&venuePhotos=1' + fsCredentials,
     method: 'GET',
     json: {},
   };
@@ -26,11 +42,20 @@ function loadVenues(locationName, venueKeyword) {
         name: venue.venue.name,
         checkins: venue.venue.stats.checkinsCount,
         users: venue.venue.stats.usersCount,
+        url: venue.venue.url,
+        rating: venue.venue.rating,
       };
+
+      if (venue.venue.photos.count) {
+        const prefix = venue.venue.photos.groups[0].items[0].prefix;
+        const suffix = venue.venue.photos.groups[0].items[0].suffix;
+        checkin.imageUrl = prefix + 'original' + suffix;
+      }
+
       checkins.push(checkin);
     }
 
-    displayCheckins(checkins);
+    displayCheckins(checkins, cardSection);
   });
 }
 
@@ -38,5 +63,5 @@ $('#search_btn').click(function () {
   $('#venue_table tbody').remove();
   const locationName = $('#location_name').val();
   const venueKeyword = $('#venue_keyword').val();
-  loadVenues(locationName, venueKeyword);
+  loadVenues(locationName, venueKeyword, $('#venue_cards'));
 });
